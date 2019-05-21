8736f4c699d2ecdcb34a86fdf41d7ed559fab42c
pdd <plsof@pdds-MacBook-Pro.local>
2019-05-21 11:38:00 +0800

    import-export add ForeignKeyWidget

--- apps/bstype/admin/service_typeAdmin.py
+++ apps/bstype/admin/service_typeAdmin.py
@@ -1,11 +1,25 @@
 from django.contrib import admin
+from import_export import fields, resources
+from import_export.widgets import ForeignKeyWidget
 from import_export.admin import ImportExportModelAdmin
 
+from bstype.models import bussiness_type
 from bstype.models import service_type
+
+
+class serviceResource(resources.ModelResource):
+    bussiness = fields.Field(column_name='bussiness',
+        attribute='bussiness',
+        widget=ForeignKeyWidget(bussiness_type, 'name'))
+
+    class Meta:
+        model = service_type
+        fields = ('bussiness', 'name', 'comments', 'update_time')
 
 
 @admin.register(service_type)
 class serviceAdmin(ImportExportModelAdmin):
+    resource_class = serviceResource
     list_display = ['bussiness', 'name', 'comments', 'update_time']
     list_filter = ['bussiness']
-    ordering = ('name',)
+
